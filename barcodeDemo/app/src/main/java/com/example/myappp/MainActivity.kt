8581f26fdb69e17e7c465abc0f25f774e4f7ec0e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-08-29 06:31:57 +0700

    update

--- barcodeDemo/app/src/main/java/com/example/myappp/MainActivity.kt
+++ barcodeDemo/app/src/main/java/com/example/myappp/MainActivity.kt
@@ -2,11 +2,59 @@ package com.example.myappp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
+import com.google.firebase.ml.vision.common.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    private fun scanBarcodes(image: FirebaseVisionImage) {
+        // [START set_detector_options]
+        val options = FirebaseVisionBarcodeDetectorOptions.Builder()
+            .setBarcodeFormats(
+                FirebaseVisionBarcode.FORMAT_QR_CODE,
+                FirebaseVisionBarcode.FORMAT_AZTEC)
+            .build()
+        // [END set_detector_options]
+        // [START get_detector]
+         val detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector(options)
+
+        // [START run_detector]
+        val result = detector.detectInImage(image)
+            .addOnSuccessListener { barcodes ->
+                // Task completed successfully
+                // [START_EXCLUDE]
+                // [START get_barcodes]
+                for (barcode in barcodes) {
+                    val bounds = barcode.boundingBox
+                    val corners = barcode.cornerPoints
+                    val rawValue = barcode.rawValue
+                    val valueType = barcode.valueType
+                    // See API reference for complete list of supported types
+                    when (valueType) {
+                        FirebaseVisionBarcode.TYPE_WIFI -> {
+                            val ssid = barcode.wifi!!.ssid
+                            val password = barcode.wifi!!.password
+                            val type = barcode.wifi!!.encryptionType
+                        }
+                        FirebaseVisionBarcode.TYPE_URL -> {
+                            val title = barcode.url!!.title
+                            val url = barcode.url!!.url
+                        }
+                    }
+                }
+                // [END get_barcodes]
+                // [END_EXCLUDE]
+            }
+            .addOnFailureListener {
+                println("error = $it")
+            }
+        // [END run_detector]
     }
 }
